c7fc5f3a40165bc91acad4a8c7888c8adc65a31a
Tinna <tinnath@Tinnas-MBP.lan>
2015-11-27 21:00:08 +0000

    ok, rugladist adeins, thurfum audvitad fyrst ad lesa inn i skra og svo ut, thannig ad eg gerdi tvo foll, eitt ut og eitt inn

--- readToFile_stuff/main.cpp
+++ readToFile_stuff/main.cpp
@@ -15,12 +15,31 @@ int main(int argc, char *argv[])
 
 void readToFile()
 {
+    ofstream out_stream;
+    string word;
+
+    out_stream.open("outfile.txt");
+    if (out_stream.fail())
+    {
+        cout << "Output file opening failed" << endl;
+        exit(1);
+    }
+    while(out_stream)
+    {
+        out_stream << word << " ";
+    }
+    out_stream.close();
+}
+
+
+void readFromFile()
+{
     ifstream in_stream("infile.txt");
     string word;
 
     if (in_stream.fail())
     {
-        cout << "Output file opening failed" << endl;
+        cout << "Input file opening failed" << endl;
         exit(1);
     }
     while(in_stream >> word)
@@ -29,5 +48,3 @@ void readToFile()
     }
     in_stream.close();
 }
-
-
