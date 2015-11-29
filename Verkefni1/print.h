0a3de2d43f5a7c428baba78b7765bfc55edfc80d
Icepawner <emilhjalta@gmail.com>
2015-11-29 16:29:50 +0000

    Create print.h
    .h file fyrir seinni klasann sem væri þá til að raða listanum upp eftir stafrofsrod, kyni, faedingarari, daudadegi etc.

---
+++ Verkefni1/print.h
@@ -0,0 +1,28 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+class Print {
+
+public: //Þetta er ekki rdy
+    Print();
+
+    Leita_i_lista();
+
+    Stafrofs_rod();
+    Stafrofs_rod_og_kyn();
+    Faedingarar_rod();
+    Daudaar_rod();
+
+    Birta_lista();
+
+    Save_file();
+};
+
+#endif // PRINT_H
