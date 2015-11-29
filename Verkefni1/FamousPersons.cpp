3184811f6e38d1b567453f36179998e0cc87f141
Tinna <tinnath@Tinnas-MBP.lan>
2015-11-29 17:53:23 +0000

    sameinadi e-d stoff, er samt ekki viss um ad eg hafi hent thvi gamla eda sett inn aftur

--- Verkefni1/FamousPersons.cpp
+++ Verkefni1/FamousPersons.cpp
@@ -15,11 +15,11 @@ FamousPersons::FamousPersons() {
 }
 
 FamousPersons::FamousPersons(string a, string b, string c, string d) {
-    string Name = a;
+    string Name = a; //thurfum vid ekki ad hafa breytunofn med litlum stofum?
     string Gender = b;
     string YearOfBirth = c;
     string YearOfDeath = d;
-} //Annaðhvort getum við get þetta svona eða /a /b /c /d
+} //Annaðhvort getum við get þetta svona eða /a /b /c /d -'new' meikar sens, lysandi nofn
 
 void setName(string newName) {
     Name = newName;
@@ -54,9 +54,15 @@ string getYearOfDeath() {
 }
 
 ostream& operator << (ostream& outs, const FamousPersons &n){
-    // Vantar
+    cout << "Name: " << Name << endl;
+    cout << "Gender: " << Gender << endl;
+    cout << "Year of birth: " << YearOfBirth << endl;
+    cout << "Year of death: " << YearOfDeath << endl;
+
+    return outs;
 }
 
 istream& operator >> (istream& ins, FamousPersons &n){
     // Vantar
+    // Viljum vid geta skrifad personur inn svona?
 }
--- Verkefni1/main.cpp
+++ Verkefni1/main.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+void readToFile();
+void readFromFile();
+
 int main() {
 
     int nr;
@@ -41,3 +44,37 @@ int main() {
 
 }
 
+void readToFile()
+{
+    ofstream out_stream;
+    string word;
+    
+    out_stream.open("outfile.txt");
+    if (out_stream.fail())
+    {
+        cout << "Output file opening failed" << endl;
+        exit(1);
+    }
+    while(out_stream)
+    {
+        out_stream << word << " ";
+    }
+    out_stream.close();
+}
+
+void readFromFile()
+{
+    ifstream in_stream("infile.txt");
+    string word;
+    
+    if (in_stream.fail())
+    {
+        cout << "Input file opening failed" << endl;
+        exit(1);
+    }
+    while(in_stream >> word)
+    {
+        cout << word << " ";
+    }
+    in_stream.close();
+}
