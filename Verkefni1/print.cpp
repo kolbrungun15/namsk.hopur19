d4cbd457a8ccb4ff2328474ffd49af928a3f7d37
Icepawner <emilhjalta@gmail.com>
2015-11-29 16:31:33 +0000

    Create print.cpp
    .cpp file fyrir aðgerðarklasann

---
+++ Verkefni1/print.cpp
@@ -0,0 +1,44 @@
+#include "print.h"
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+Print::Print() {
+    //Vantar
+}
+
+
+Print::Leita_i_lista(){
+    //Vantar
+}
+
+
+Print::Stafrofs_rod(){
+    //Vantar
+}
+
+Print::Stafrofs_rod_og_kyn(){
+    //Vantar
+}
+
+Print::Faedingarar_rod(){
+    //Vantar
+}
+
+Print::Daudaar_rod(){
+    //Vantar
+}
+
+
+Print::Birta_lista(){
+    //Vantar
+}
+
+
+Print::Save_file(){
+    //Vantar
+}
