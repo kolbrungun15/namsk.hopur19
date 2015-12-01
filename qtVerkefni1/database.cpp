3a26497bae37e9c2386bfb354db5fb3689c7a910
Tinna <tinnath@Tinnas-MacBook-Pro.local>
2015-12-01 16:59:10 +0000

    breytingar a database, mogulega slaemar

--- qtVerkefni1/database.cpp
+++ qtVerkefni1/database.cpp
@@ -5,6 +5,39 @@
 #include <fstream>
 #include <cstdlib>
 
-Database::Database()
+Database::Database(string filename)
 {
+
+}
+
+void Database::add(FamousPersons pers)
+{
+    vec.push_back(pers);
+}
+
+void Database::vectorToFile(vector <FamousPersons> &vec)
+{
+    for (unsigned int i = 0; i < vec.size(); i++)
+    {
+        readTofile(vec[i]);
+    }
+}
+
+void Database::readTofile(FamousPersons &temp)
+{
+    ofstream out_stream;
+
+    out_stream.open("csLeagends.txt", ios::out | ios::app);
+    if (out_stream.fail())
+    {
+        cout << "Output file opening failed" << endl;
+        exit(1);
+    }
+    out_stream << "First name: " << temp.getFirstName() << endl;
+    out_stream << "Last name: " << temp.getLastName() << endl;
+    out_stream << "Gender (male/female): " << temp.getGender() << endl;
+    out_stream << "Year of birth: " << temp.getYearOfBirth() << endl;
+    out_stream << "Year of death: " << temp.getYearOfDeath() << endl;
+    out_stream << endl;
+    out_stream.close();
 }
